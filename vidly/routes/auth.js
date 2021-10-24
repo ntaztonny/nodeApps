b1e8fdede563c08f257445eb9a2d5d6705fafda5
ntaztonny <atozpp@yahoo.com>
2021-10-24 02:02:00 +0200

    authetication testing

--- vidly/routes/auth.js
+++ vidly/routes/auth.js
@@ -4,23 +4,39 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const Joi = require("joi");
 
 router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  // const { error } = validate(req.body);
+  // if (error) return res.status(400).send(error.details[0].message);
 
   let user = await UserSchmaModel.findOne({ email: req.body.email });
+  console.log(req.body.name);
+  console.log(req.body.password);
+  console.log(user);
   if (!user) return res.status(400).send("Ivalid email or password");
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Ivalid email or password");
+
+  res.send("true");
 });
 
 function validate(req) {
-  const Schema = {
+  const schema = {
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(5).max(255).required(),
   };
+
+  return Joi.valid(req, schema);
 }
 
+(req) => {
+  const schema = {
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  };
+
+  return Joi.valid(req, schema);
+};
 module.exports = router;
